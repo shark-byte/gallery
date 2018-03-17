dbf39a3e947c46d5e655b1ba791bffaa40df2d44
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-03-17 16:33:11 -0700

    sets up node-postgres

--- data/pgSchema.sql
+++ data/pgSchema.sql
@@ -1,12 +1,22 @@
+DROP TABLE IF EXISTS restaurants
+DROP TABLE IF EXISTS photos
+DROP TABLE IF EXISTS reviews
+
 -- CREATE SCHEMA gallery;
 
--- CREATE TABLE restaurants (
---    place_id   NUMERIC NOT NULL,
---    place_name VARCHAR(30) NOT NULL,
---    CONSTRAINT employees_pk PRIMARY KEY (place_id)
--- );
+CREATE TABLE restaurants (
+   place_id NUMERIC NOT NULL,
+   place_name VARCHAR(30) NOT NULL,
+   CONSTRAINT employees_pk PRIMARY KEY (place_id)
+);
 
-INSERT INTO restaurants VALUES(
-  1,
-  'Burger King'
+CREATE TABLE photos (
+  url VARCHAR(200) NOT NULL,
+  width SMALLINT NOT NULL,
+  height SMALLINT NOT NULL
+);
+
+CREATE TABLE reviews (
+  name VARCHAR(50) NOT NULL,
+  avatar VARCHAR(200)
 );
